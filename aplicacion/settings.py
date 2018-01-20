fa7cb411fd607089f1c4629cb8e4eb52f505551c
humbertoandueza <humbertoanduezaa@gmail.com>
2018-01-20 09:14:11 -0430

    arrefgl

--- aplicacion/settings.py
+++ aplicacion/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = '+s!7-bs!e#l)gwm#2ckpbq=(%4v%4qx=am-*q+1km3hjbs8nad'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #<<<<<<< HEAD
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['humberto.pythonanywhere.com']
 #>>>>>>> d9c1d0b5d486a68905c8dfe590338b2f2372e74d
 
 AUTH_USER_MODEL = 'accounts.Users'
@@ -81,9 +81,11 @@ WSGI_APPLICATION = 'aplicacion.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_school',
-        'USER': 'root',
-        'PASSWORD': '',
+        'NAME': 'humberto$bd_school',
+        'USER': 'humberto',
+        'PASSWORD': '01051998a.',
+        'HOST': 'humberto.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 """
@@ -155,11 +157,11 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'humbertoanduezaa@gmail.com'
 EMAIL_HOST_PASSWORD = '01051998a'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-"""
+
 #SESSION EXPIRED
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 120 # set just 10 seconds to test
 SESSION_SAVE_EVERY_REQUEST = True
 
 #HTTPS
-SECURE_SSL_REDIRECT = True"""
+SECURE_SSL_REDIRECT = True
