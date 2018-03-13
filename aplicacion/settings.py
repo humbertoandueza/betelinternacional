5f781448e5f8d51a42c341302393969dc135dbce
humbertoandueza <humbertoanduezaa@gmail.com>
2018-03-13 14:11:40 -0430

    cambios

--- aplicacion/settings.py
+++ aplicacion/settings.py
@@ -23,27 +23,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+s!7-bs!e#l)gwm#2ckpbq=(%4v%4qx=am-*q+1km3hjbs8nad'
 
-
-<<<<<<< HEAD
-=======
-=======
-# SECURITY WARNING: don't run with debug turned on in production!
-<<<<<<< HEAD
-
-=======
-#<<<<<<< HEAD
->>>>>>> 2e500457c73cce9fe59bda1daf346ed32d663dec
->>>>>>> 88b1e3ccdd144ff10f1656278c3e1b7068dba69d
->>>>>>> 173e1c494cbcfb57a1d5397da78189c0c594397e
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-#ALLOWED_HOSTS = ['iglesiabetel.pythonanywhere.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iglesiabetel.pythonanywhere.com']
+#ALLOWED_HOSTS = []
 
 
 AUTH_USER_MODEL = 'accounts.Users'
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -53,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'datos',
     'accounts',
+    'mail_templated',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -91,10 +81,10 @@ WSGI_APPLICATION = 'aplicacion.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_school',
-        'USER': 'root',
-        'PASSWORD': '',
-        #'HOST':'iglesiabetel.mysql.pythonanywhere-services.com',
+        'NAME': 'iglesiabetel$default',
+        'USER': 'iglesiabetel',
+        'PASSWORD': '01051998a',
+        'HOST':'iglesiabetel.mysql.pythonanywhere-services.com',
     }
 }
 
@@ -155,10 +145,9 @@ EMAIL_HOST_USER = 'humbertoanduezaa@gmail.com'
 EMAIL_HOST_PASSWORD = '01051998a'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #SESSION EXPIRED
-"""
+
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 120 # set just 10 seconds to test
 SESSION_SAVE_EVERY_REQUEST = True
-#HTTPS
 
-SECURE_SSL_REDIRECT = True"""
+SECURE_SSL_REDIRECT = True
