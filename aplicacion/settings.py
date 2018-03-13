28c0459aba95c88ac0bc909abfe32d3a72454dc2
humbertoandueza <humbertoanduezaa@gmail.com>
2018-03-13 17:50:56 -0430

    cambioss

--- aplicacion/settings.py
+++ aplicacion/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '+s!7-bs!e#l)gwm#2ckpbq=(%4v%4qx=am-*q+1km3hjbs8nad'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = ['iglesiabetel.pythonanywhere.com']
+ALLOWED_HOSTS = ['iglesiabetel.pythonanywhere.com']
+#ALLOWED_HOSTS = []
 
 
 AUTH_USER_MODEL = 'accounts.Users'
@@ -81,9 +81,10 @@ WSGI_APPLICATION = 'aplicacion.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_school',
-        'USER': 'root',
-        'PASSWORD': '',
+        'NAME': 'iglesiabetel$default',
+        'USER': 'iglesiabetel',
+        'PASSWORD': '01051998a',
+        'HOST':'iglesiabetel.mysql.pythonanywhere-services.com',
     }
 }
 
@@ -149,4 +150,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 120 # set just 10 seconds to test
 SESSION_SAVE_EVERY_REQUEST = True
 
-#SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
+
+SECURE_SSL_REDIRECT = True
+
