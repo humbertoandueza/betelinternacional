b2f77d8e5e545b41aacafd38ba49bd6d7805fb56
humbertoandueza <humbertoanduezaa@gmail.com>
2018-03-14 10:02:52 +0000

    arrreglo

--- aplicacion/settings.py
+++ aplicacion/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+s!7-bs!e#l)gwm#2ckpbq=(%4v%4qx=am-*q+1km3hjbs8nad'
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['iglesiabetel.pythonanywhere.com']
 #ALLOWED_HOSTS = []
